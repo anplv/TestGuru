61c9f07e031b34281b975f871c4ef072bbde6338
vslv <vslv.anpilov@gmail.com>
2022-08-02 22:49:03 +0300

    feat: add default values

--- db/migrate/20220802181306_create_tests.rb
+++ db/migrate/20220802181306_create_tests.rb
@@ -2,7 +2,7 @@ class CreateTests < ActiveRecord::Migration[6.1]
   def change
     create_table :tests do |t|
       t.string :title, null: false
-      t.integer :level, null: false
+      t.integer :level, null: false, default: 0
       add_foreign_key :categories, :tests
 
       t.timestamps
--- db/migrate/20220802183231_create_answers.rb
+++ db/migrate/20220802183231_create_answers.rb
@@ -1,6 +1,7 @@
 class CreateAnswers < ActiveRecord::Migration[6.1]
   def change
     create_table :answers do |t|
+      t.boolean :correct, default: false
       t.string :body, null: false
       add_foreign_key :questions, :answers
 
