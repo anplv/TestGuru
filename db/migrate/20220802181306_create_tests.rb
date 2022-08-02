9e7445c970cf35ea00fd79e1d640df88cf7f02c0
vslv <vslv.anpilov@gmail.com>
2022-08-02 22:32:37 +0300

    feat: add foreign keys in migrations

--- db/migrate/20220802181306_create_tests.rb
+++ db/migrate/20220802181306_create_tests.rb
@@ -3,6 +3,7 @@ class CreateTests < ActiveRecord::Migration[6.1]
     create_table :tests do |t|
       t.string :title
       t.integer :level
+      add_foreign_key :categories, :tests
 
       t.timestamps
     end
--- db/migrate/20220802183231_create_answers.rb
+++ db/migrate/20220802183231_create_answers.rb
@@ -2,6 +2,7 @@ class CreateAnswers < ActiveRecord::Migration[6.1]
   def change
     create_table :answers do |t|
       t.string :body
+      add_foreign_key :questions, :answers
 
       t.timestamps
     end
