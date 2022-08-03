89d3139620ab52fd45129625dce8931ce728bc14
vslv <vslv.anpilov@gmail.com>
2022-08-03 22:58:10 +0300

    fix: fix migration

--- db/migrate/20220802183021_create_questions.rb
+++ db/migrate/20220802183021_create_questions.rb
@@ -2,8 +2,9 @@ class CreateQuestions < ActiveRecord::Migration[6.1]
   def change
     create_table :questions do |t|
       t.string :body, null: false
-      t.references :tests, foreign_key: true
-                   t.timestamps
+      t.references :test, foreign_key: true
+
+      t.timestamps
     end
   end
 end
--- db/migrate/20220802183231_create_answers.rb
+++ db/migrate/20220802183231_create_answers.rb
@@ -3,7 +3,7 @@ class CreateAnswers < ActiveRecord::Migration[6.1]
     create_table :answers do |t|
       t.boolean :correct, default: false
       t.string :body, null: false
-      t.references :questions, foreign_key: true
+      t.references :question, foreign_key: true
 
       t.timestamps
     end
